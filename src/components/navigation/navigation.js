e621cf415cbe32685fb049e94ef2955d50468fa7
Lachlan Peacock <lachlan.peacock@gmail.com>
2020-10-30 11:41:40 +1100

    Reconfigure as Create-React-App Project

---
+++ src/components/navigation/navigation.js
@@ -0,0 +1,17 @@
+import "./navigation.css";
+
+function Navigation() {
+  // list items are placeholders. Replace with menu-item components once built
+  return (
+    <div className="Navigation">
+      <ul>
+        <li>About Me</li>
+        <li>Socials</li>
+        <li>Portfolio</li>
+        <li>Get In Touch</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navigation;
