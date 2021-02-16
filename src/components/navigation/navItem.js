925b04b7dd5c5008a3b30594f80f87b31fec9921
Lachlan Peacock <lachlan.peacock@gmail.com>
2021-02-16 12:28:41 +1100

    Lift-up state of NavItem and restyle

--- src/components/navigation/navItem.js
+++ src/components/navigation/navItem.js
@@ -4,20 +4,19 @@ import "./navItem.css";
 class NavItem extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { selected: false };
 
     this.handleClick = this.handleClick.bind(this);
   }
 
+
   handleClick(e) {
     e.preventDefault();
-    this.setState({ selected: !this.state.selected });
-    console.log(this.props.title);
-    //console.log("Nav item clicked");
+    this.props.handler(this.props.title)
+
   }
 
   render() {
-    if (this.state.selected) {
+    if (this.props.style) {
       return (
         <li className="Selected" onClick={this.handleClick}>
           {this.props.title}
