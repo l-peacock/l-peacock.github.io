0c51dc83124d845f6a9d97e369b58be4b97bdfbe
Lachlan Peacock <lachlan.peacock@gmail.com>
2021-02-16 12:27:12 +1100

    Lift-up state of NavItem to Navigation

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,24 +1,48 @@
+import { Component, useRef } from "react";
 import "./navigation.css";
 import NavItem from "./navItem";
 
-function Navigation() {
-  // list items are placeholders. Replace with menu-item components once built
-  return (
-    <div className="Navigation">
+const navItems = [{"name":"Home"},{"name":"About Me"},{"name":"Socials"},{"name":"Portfolio"},{"name":"Get In Touch"}]
+class Navigation extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.handler = this.handler.bind(this)
+    this.state = {selected: "Home"}
+  }
+
+  handler(name) {
+    this.setState({selected: name})
+  }
+
+  render () {
+
+    //https://stackoverflow.com/questions/32157286/rendering-react-components-from-array-of-objects
+    const NavList = ({items}) => (
+      <>
+        {items.map(item => {
+          if( this.state.selected === item.name ){
+           return <NavItem title={item.name} key={item.name} handler={this.handler} style="selected"/>
+          } else {
+              return <NavItem title={item.name} key={item.name} handler={this.handler}/>
+          }
+        })}
+      </>
+    );
+
+    
+    return <div className="Navigation">
       <div className="Message">
         <h1>Hi!</h1>
         <h2>I'm Lachie</h2>
       </div>
 
-      <ul>
-        <NavItem title="Home" />
-        <NavItem title="About Me" />
-        <NavItem title="Socials" />
-        <NavItem title="Portfolio" />
-        <NavItem title="Get In Touch" />
+      <ul >
+        <NavList items={navItems}/>
       </ul>
     </div>
-  );
+  }
 }
 
 export default Navigation;
