1f3044a0b48bc2af9041df5371978aa6d7a24773
Lachlan Peacock <lachlan.peacock@gmail.com>
2021-02-16 19:56:17 +1100

    Add placeholder content

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -1,12 +1,31 @@
 import "./content.css";
 
 function Content(props) {
-  //console.log(props)
+  
+  let key = props.page.toLowerCase();
+  console.log(key)
+
   return (
     <div className="Content">
-      <p>Current page is: {props.page}</p>
+      <p>{content[key]}</p>
     </div>
   );
+}
+
+/**
+ * Content is currently a placeholder. This switching mechanism can be used to pass a unique react component for each page type.
+ */
+const content = {
+  home: `This is the content for the HOME page.
+  This is just some basic text. Will experiment with other layouts later.`,
+  'about me': `This is the content for the ABOUT ME page.
+  This is just some basic text. Will experiment with other layouts later.`,
+  socials: `This is the content for the SOCIALS page.
+  This is just some basic text. Will experiment with other layouts later.`,
+  portfolio: `This is the content for the PORTFOLIO page.
+  This is just some basic text. Will experiment with other layouts later.`,
+  'get in touch': `This is the content for the GET IN TOUCH page.
+  This is just some basic text. Will experiment with other layouts later.`
 }
 
 export default Content;
